3a2dd881cfe38c05890720f1189b6732ab4451a0
Chris Patrick <atomize101@gmail.com>
2020-11-12 09:31:27 -0500

    Creating and styling input fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,84 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { TextField, Typography, Button, Grid, Box } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import Navbar from './Navbar';
+import { Translate } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+	form: {
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		position: 'absolute',
+	},
+}));
+
+const InputField = withStyles({
+	root: {
+		'& label.mui-focused': {
+			color: 'tomato',
+		},
+		'& label': {
+			color: 'tan',
+		},
+		'& .MuiOutlinedInput-root': {
+			'& fieldset': {
+				borderColor: 'tan',
+			},
+			'&:hover fieldset': {
+				borderColor: 'tan',
+			},
+			'& .Mui-focused fieldset': {
+				borderColor: 'tan',
+			},
+		},
+	},
+})(TextField);
 
 const Contact = () => {
-	return <div>HELLO!!!</div>;
+	const classes = useStyles();
+
+	return (
+		<Box component="div">
+			<Navbar />
+			<Grid container justify="center">
+				<Box component="form" className={classes.form}>
+					<Typography variant="h5">Contact me</Typography>
+					<InputField
+						fullwidth={true}
+						label="Name"
+						variant="outlined"
+						inputProps={{ style: { color: 'white' } }}
+						margin="dense"
+						size="medium"
+					/>
+					<br />
+					<InputField
+						fullwidth={true}
+						label="Email"
+						variant="outlined"
+						inputProps={{ style: { color: 'white' } }}
+						margin="dense"
+						size="medium"
+					/>
+					<br />
+					<InputField
+						fullwidth={true}
+						label="Company Name"
+						variant="outlined"
+						inputProps={{ style: { color: 'white' } }}
+						margin="dense"
+						size="medium"
+					/>
+					<br />
+					<Button variant="outlined" fullWidth={true} sendIcon={<SendIcon />}>
+						Contact Me
+					</Button>
+				</Box>
+			</Grid>
+		</Box>
+	);
 };
 
 export default Contact;
