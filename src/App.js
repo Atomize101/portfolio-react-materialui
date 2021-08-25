9bfb707ac60b389e728e9614511d96063e76834e
Chris Patrick <atomize101@gmail.com>
2021-08-25 13:12:26 -0400

    Removing resume

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import { Route } from 'react-router-dom';
-import Home from './components/index';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import imageUrl from './coffee-apple.jpg';
-import Contact from './components/Contact';
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./components/index";
+/* import Resume from "./components/Resume"; */
+import Portfolio from "./components/Portfolio";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import imageUrl from "./coffee-apple.jpg";
+import Contact from "./components/Contact";
 
 function App() {
-	return (
-		<div className="App" style={{ backgroundImage: `url(${imageUrl})` }}>
-			<>
-				<CssBaseline />
-				<Route exact path="/" component={Home} />
-				<Route path="/resume" component={Resume} />
-				<Route path="/portfolio" component={Portfolio} />
-				<Route path="/contact" component={Contact} />
-			</>
-		</div>
-	);
+  return (
+    <div className="App" style={{ backgroundImage: `url(${imageUrl})` }}>
+      <>
+        <CssBaseline />
+        <Route exact path="/" component={Home} />
+        {/* <Route path="/resume" component={Resume} /> */}
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+      </>
+    </div>
+  );
 }
 
 export default App;
